2680d186cd3f82d7caad90cb7e871b8cd024ec19
landon.ausauce.0327@gmail.com <amirul@goprop.ai>
2026-03-12 10:15:36 +0800

    Revert "bottom menu & side weather bar (Part 1)"
    This reverts commit 79a07afd3b3f69abb26cc86019a4e597d751c528.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import BottomMenu from "./components/menuWidgets/BottomMenu";
 import MainLoading from "./pages/MainLoading";
 import MainScene from "./scenes/MainScene";
-import { FullBottomWidget } from "./components/fullBottomWidgets/FullBottomWidget";
-import RightWidget from "./components/geoclimateWidgets/RightWidget";
 
 function App() {
-  const [bottomMenu, setBottomMenu] = useState<string>("home");
-
-  const [location, setLocation] = useState(() => {
-      const params = new URLSearchParams(window.location.search);
-      return params.get("location") || "kl";
-  });
-
   return (
     <>
       <MainLoading />
       <MainScene />
-      <div className="widget">
-            <RightWidget mapType="3d" location="kl" />
-            <BottomMenu />
-      </div>
     </>
   );
 }

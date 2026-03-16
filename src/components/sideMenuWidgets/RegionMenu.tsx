7e6f5425a4592b56af9ff90bd572a525fcdf419d
landon.ausauce.0327@gmail.com <amirul@goprop.ai>
2026-03-16 18:45:27 +0800

    RegionMenu: state management Zustand - removing prop drilling, etc.

--- src/components/sideMenuWidgets/RegionMenu.tsx
+++ src/components/sideMenuWidgets/RegionMenu.tsx
@@ -1,4 +1,8 @@
-// import { useAppContext } from "@context/AppContext";
+import { useLocationStore } from "../../stores/locationStore";
+import { useProjectStore } from "../../stores/projectStore";
+import { useRegionMenuStore } from "../../stores/regionMenuStore";
+import { useSideMenuStore } from "../../stores/sideMenuStore";
+import "./styles/RegionMenu.css";
 
 export interface RegionItem {
     name: string;
@@ -7,48 +11,22 @@ export interface RegionItem {
     iconHighlight: string;
 }
 
-interface RegionProp {
-    location: string;
-    regionMenu: string;
-    setRegionMenu: (menu: string) => void;
-}
-
-const RegionMenu = ({ location, regionMenu, setRegionMenu }: RegionProp) => {
-//   const {
-//     regionMenu,
-//     setRegionMenu,
-//     setSideMenu,
-//     location,
-//     setLocation,
-//     setSelectedProject,
-//   } = useAppContext();
+const RegionMenu = () => {
     const regionDir = "src/assets/icons/RegionMenu";
+    const { setSelectedMenu: setSideMenu } = useSideMenuStore();
+    const { selectedMenu: regionMenu, setSelectedMenu: setRegionMenu } = useRegionMenuStore();
+    const { location: location, setLocation: setLocation } = useLocationStore();
+    const { setProject: setSelectedProject } = useProjectStore();
+
+    function newRegionItem(name: string, location: string, icon: string, iconHighlight: string) {
+        return { name, location, icon, iconHighlight };
+    }
 
     const regionItems: Record<string, RegionItem> = {
-        KL: {
-            name: "Kuala Lumpur",
-            location: "kl",
-            icon: `${regionDir}/KL.svg`,
-            iconHighlight: `${regionDir}/KL-highlight.svg`,
-        },
-        SLG: {
-            name: "Selangor",
-            location: "selangor",
-            icon: `${regionDir}/Selangor.svg`,
-            iconHighlight: `${regionDir}/Selangor-highlight.svg`,
-        },
-        GH: {
-            name: "Genting Highlands",
-            location: "genting",
-            icon: `${regionDir}/Genting.svg`,
-            iconHighlight: `${regionDir}/Genting-highlight.svg`,
-        },
-        JB: {
-            name: "Johor Bahru",
-            location: "jb",
-            icon: `${regionDir}/JB.svg`,
-            iconHighlight: `${regionDir}/JB-highlight.svg`,
-        },
+        KL: newRegionItem("Kuala Lumpur", "kl", `${regionDir}/KL.svg`, `${regionDir}/KL-highlight.svg`),
+        SLG: newRegionItem("Selangor", "selangor", `${regionDir}/Selangor.svg`, `${regionDir}/Selangor-highlight.svg`),
+        GH: newRegionItem("Genting Highlands", "genting", `${regionDir}/Genting.svg`, `${regionDir}/Genting-highlight.svg`),
+        JB: newRegionItem("Johor Bahru", "jb", `${regionDir}/JB.svg`, `${regionDir}/JB-highlight.svg`),
     };
 
     return (
@@ -60,26 +38,21 @@ const RegionMenu = ({ location, regionMenu, setRegionMenu }: RegionProp) => {
                         <div
                             className={`region-menu__item ${
                                 (regionMenu === item.location) ? "active" : ""
-                            } ${item.location === "selangor" ? "disable" : ""}`}
+                            } ${(item.location === "selangor") ? "disable" : ""}`}
                             key={key}
                             onClick={() => {
                                 if (location === item.location) {
                                     setRegionMenu(item.location);
-                                    //setSideMenu("project");
+                                    setSideMenu("project");
                                 } else {
                                     setRegionMenu(item.location);
-                                    //   setLocation(item.location);
-                                    //   setSelectedProject(null);
+                                    setLocation(item.location);
+                                    setSelectedProject(null);
                                 }
                             }}
                         >
                             <div className="region-menu__item-icon">
-                                <img
-                                    src={
-                                        regionMenu === item.name ? item.iconHighlight : item.icon
-                                    }
-                                    alt={item.name}
-                                />
+                                <img src={(regionMenu === item.name) ? item.iconHighlight : item.icon} alt={item.name} />
                             </div>
                             <div className="region-menu__item-text">{item.name}</div>
                         </div>

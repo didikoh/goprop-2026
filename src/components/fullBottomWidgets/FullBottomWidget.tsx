ca2daf3486768dec047c330fe2cb77cc2473a3ab
landon.ausauce.0327@gmail.com <amirul@goprop.ai>
2026-03-19 18:14:30 +0800

    Bottom Menu: UI enhancements, state-managements (zustand), refactoring, etc.

--- src/components/fullBottomWidgets/FullBottomWidget.tsx
+++ src/components/fullBottomWidgets/FullBottomWidget.tsx
@@ -3,58 +3,18 @@ import RightWidget from "../geoclimateWidgets/RightWidget";
 import SideMenu from "../sideMenuWidgets/SideMenu";
 import AmenitiesMenu from "../amenitiesMenuWidgets/AmenitiesMenu";
 import { useBottomMenuStore } from "../../stores/bottomMenuStore";
-import type { ProjectModel } from "../../api/projects/ProjectModel";
 
-interface ProjectListProp {
-    projectsList: ProjectModel[];
-    // selectedProject: ProjectModel | null; 
-    // setSelectedProject: (project: ProjectModel | null) => void;
-}
-
-export function FullBottomWidget({ projectsList }: ProjectListProp) {
+export function FullBottomWidget() {
     // const [location, setLocation] = useState<string>("kl");
     const { selectedMenu: bottomMenu } = useBottomMenuStore(); // "home" | "projects" | "chat" | "amenities" | "whatsapp"
     //const { selectedMenu: sideMenu, setSelectedMenu: setSideMenu } = useSideMenuStore(); // "region" | "project" | "projectInfo" | "landmarkInfo"
     // const [amenities, setAmenities] = useState<any>("");
     // const [subMenu, setSubMenu] = useState<string>("home");
     // const [mapType, setMapType] = useState("3d");
-
-    const sideMenuProps = {
-        projectsList,
-        // sideMenu,
-        // regionMenu,
-        // setRegionMenu,
-        // location,
-        // setLocation,
-        // setBottomMenu,
-        // setSideMenu,
-        // selectedProject,
-        // setSelectedProject,
-        // closeBtn: () => {
-        //     setSelectedProject(null);
-        //     setBottomMenu("home");
-        //     setSideMenu("region");
-        // }
-    };
-
-    /*const amenitiesProps = {
-        //location,
-        distance: -1,
-        //subMenu,
-        // amenities,
-        // setAmenities
-    };
-
-    const rightWidgetProps = {
-        //mapType,
-        location,
-        //setMapType
-    };*/
-
     
     return (
         <div className="widget">
-            {(bottomMenu === "projects") && <SideMenu {...sideMenuProps} />}
+            {(bottomMenu === "projects") && <SideMenu />}
             {(bottomMenu === "amenities") && <AmenitiesMenu />}
             <RightWidget />
             <BottomMenu />

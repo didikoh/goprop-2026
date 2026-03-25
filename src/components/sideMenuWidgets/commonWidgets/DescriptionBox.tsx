5e378d212458543bf84469dc89ec5d423bedac19
landon.ausauce.0327@gmail.com <amirul@goprop.ai>
2026-03-25 16:01:04 +0800

    Description box for project/landmark description, Developer box for project developer's description, etc.

--- src/components/sideMenuWidgets/commonWidgets/DescriptionBox.tsx
+++ src/components/sideMenuWidgets/commonWidgets/DescriptionBox.tsx
@@ -1,18 +1,38 @@
 
 
 interface DescriptionProp {
-    className01: string;
-    className02: string;
+    isLandmark?: boolean;
+    className02?: string;
+    titleLbl00?: string;
     titleLbl?: string;
     data: string;
 }
 
-export function DescriptionBox({ className01, className02, titleLbl, data }: DescriptionProp) {
+export function SubDescriptionBox({ className02, titleLbl, data }: DescriptionProp) {
     return (
-        <div className={className01}>
-            <div className={className02}>
-                {(titleLbl) ? <strong>Description</strong> : null}
-                <p>{data}</p>
+        <div className={className02}>
+            {(titleLbl) ? <strong>{titleLbl}</strong> : null}
+            <p>{data}</p>
+        </div>
+    );
+}
+
+export function DescriptionBox({ isLandmark, titleLbl, data }: DescriptionProp) {
+    return (
+        <div className={`${(isLandmark) ? 'landmark-info' : 'project-Info'}__info${(isLandmark) ? '3' : '4'}`}>
+            <SubDescriptionBox className02={`${(isLandmark) ? 'landmark-info' : 'project-Info'}__description`} titleLbl={titleLbl} data={data} />
+        </div>
+    );
+}
+
+export function DeveloperBox({ className02, titleLbl, data }: DescriptionProp) {
+    return (
+        <div className="project-Info__info7">
+            <div className="project-Info__developer">
+              <div className="project-Info__developer-title">
+                <strong>Developer</strong>
+              </div>
+              <SubDescriptionBox className02={className02} titleLbl={titleLbl} data={data} />
             </div>
         </div>
     );

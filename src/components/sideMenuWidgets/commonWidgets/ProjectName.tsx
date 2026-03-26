67e51412e115a2e0fe9f20c1a4529287cd8f5826
landon.ausauce.0327@gmail.com <amirul@goprop.ai>
2026-03-26 16:08:19 +0800

    ProjectName widget revised & simplified for landmark & project

--- src/components/sideMenuWidgets/commonWidgets/ProjectName.tsx
+++ src/components/sideMenuWidgets/commonWidgets/ProjectName.tsx
@@ -1,18 +1,17 @@
 
-
 interface ProjectNameProp {
+    isLandmark: boolean;
     name: string;
-    className: string;
     prevLandmark: () => void;
     nextLandmark: () => void;
 }
 
-export function ProjectName({ name, className, prevLandmark, nextLandmark }: ProjectNameProp) {
+export function ProjectName({ isLandmark, name, prevLandmark, nextLandmark }: ProjectNameProp) {
     return (
-        <div className={className}>
-            <i className="fa-solid fa-chevron-left" onClick={prevLandmark} />
+        <div className={`${(isLandmark) ? 'landmark-info' : 'project-Info'}__name`}>
+            <i className={`fa-solid fa-chevron-left`} onClick={prevLandmark} />
             <h2>{name}</h2>
-            <i className="fa-solid fa-chevron-right" onClick={nextLandmark} />
+            <i className={`fa-solid fa-chevron-right`} onClick={nextLandmark} />
         </div>
     );
 }
